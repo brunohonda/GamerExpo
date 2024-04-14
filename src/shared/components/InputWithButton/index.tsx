4007400e788dce4b36e61d4f4c0aa0ec826bae90
Bruno Honda <bruno.honda@live.com>
2024-04-14 19:24:38 -0300

    feat(input-with-button): adds on-click event

--- src/shared/components/InputWithButton/index.tsx
+++ src/shared/components/InputWithButton/index.tsx
@@ -1,14 +1,15 @@
-import { Image, ImageSourcePropType, TextInputProps } from "react-native";
+import { GestureResponderEvent, Image, ImageSourcePropType, TextInputProps } from "react-native";
 import { Button, Input as Field, InputContainer } from "./styles";
 
 interface InputWithButtonProps extends TextInputProps {
-  iconSource: ImageSourcePropType
+  iconSource: ImageSourcePropType;
+  onClick?: ((event: GestureResponderEvent) => void);
 }
 
 export function InputWithButton({ ...props }: Readonly<InputWithButtonProps>) {
   return <InputContainer>
-    <Field {...props} />
-    <Button>
+    <Field {...props } />
+    <Button onPress={ props.onClick }>
       <Image source={ props.iconSource }></Image>
     </Button>
   </InputContainer>
